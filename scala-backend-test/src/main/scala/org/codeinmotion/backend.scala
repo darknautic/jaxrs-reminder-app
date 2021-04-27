58b6af7156877c3a96359d36febf590cc4bffa04
darknautic <nautikos@codeinmotion.org>
2021-04-27 09:25:48 -0400

    adding maven def 1

---
+++ scala-backend-test/src/main/scala/org/codeinmotion/backend.scala
@@ -0,0 +1,11 @@
+package org.codeinmotion
+
+class Backend {
+
+  def getSquareString(input: Double): String = {
+    val square = input * input
+    square.toString
+  }
+  //println(getSquareString(2.5))
+
+}
